b374ec8383cba6fbaec78c5e051a93c40a0d1d5b
Puneet Mehrotra <p.puneetmehrotra@outlook.com>
2025-03-21 21:52:01 -0700

    added code to capture and report more rusage fields

--- include/Util.h
+++ include/Util.h
@@ -91,21 +91,35 @@ class comma_numpunct : public std::numpunct<char>
 };
 
 class MemoryCounter {
- public:
-    MemoryCounter() {
-        getrusage(RUSAGE_SELF, &__memory);
-        __previous_mem = __memory.ru_maxrss;
-    }
+public:
+  MemoryCounter() {
+    getrusage(RUSAGE_SELF, &__memory);
+    __previous_mem = __memory.ru_maxrss;
+    __maj_faults = __memory.ru_majflt;
+    __min_faults = __memory.ru_minflt;
+    __inblock = __memory.ru_inblock;
+    __oublock = __memory.ru_oublock;
+  }
 
-    ~MemoryCounter() {
-        getrusage(RUSAGE_SELF, &__memory);
-        uint64_t used_mem = __memory.ru_maxrss - __previous_mem;
-        printf("MemoryCounter: %lu MB -> %lu MB, %lu MB total\n", __previous_mem/1024, __memory.ru_maxrss/1024, used_mem/1024);
-    }
+  ~MemoryCounter() {
+    getrusage(RUSAGE_SELF, &__memory);
+    uint64_t used_mem = __memory.ru_maxrss - __previous_mem;
+    uint64_t maj_faults = __memory.ru_majflt - __maj_faults;
+    uint64_t min_faults = __memory.ru_minflt - __min_faults;
+    uint64_t inblock = __memory.ru_inblock - __inblock;
+    uint64_t oublock = __memory.ru_oublock - __oublock;
+    printf("MemoryCounter: %lu MB -> %lu MB, %lu MB total\n", __previous_mem/1024, __memory.ru_maxrss/1024, used_mem/1024);
+    printf("MemoryCounter: %lu major faults, %lu minor faults\n", maj_faults, min_faults);
+    printf("MemoryCounter: %lu block input operations, %lu block output operations\n", inblock, oublock);
+  }
 
- private:
-     struct rusage __memory;
-     uint64_t __previous_mem;
+private:
+  struct rusage __memory;
+  uint64_t __previous_mem;
+  uint64_t __maj_faults;
+  uint64_t __min_faults;
+  uint64_t __inblock;
+  uint64_t __oublock;
 };
 
 #define declare_memory_counter \
